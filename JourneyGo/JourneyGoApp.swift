781e251510834f66830c5a2234295fdeaccb663a
Michelle <aryanto.michelle@gmail.com>
2024-05-19 20:44:51 +0700

    Initial Commit

---
+++ JourneyGo/JourneyGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyGoApp.swift
+//  JourneyGo
+//
+//  Created by Michelle Angela Aryanto on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JourneyGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
